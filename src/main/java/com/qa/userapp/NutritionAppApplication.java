1641d4163cdf03c5f6d54e7aebfe34e949819907
london-ay <aysun.uk2017@gmail.com>
2022-02-01 10:13:24 +0000

    initialized project

---
+++ src/main/java/com/qa/userapp/NutritionAppApplication.java
@@ -0,0 +1,13 @@
+package com.qa.userapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/userapp/NutritionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.userapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
