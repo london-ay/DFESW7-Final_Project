36b62b134896a7bfde43ede109c7c24732bcc467
london-ay <aysun.uk2017@gmail.com>
2022-02-02 12:55:30 +0000

    created the food controller class

---
+++ src/main/java/com/qa/userapp/controller/FoodController.java
@@ -0,0 +1,38 @@
+package com.qa.userapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.userapp.data.FoodEntity;
+import com.qa.userapp.service.FoodService;
+
+@RestController
+@RequestMapping(path = "/food")
+public class FoodController {
+	
+	private FoodService service;
+	
+	@Autowired
+	public FoodController(FoodService service) {
+		this.service = service;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<FoodEntity>> getFoods() {
+		ResponseEntity<List<FoodEntity>> foods = ResponseEntity.ok(service.getAllFoods());
+		return foods;
+	}
+	
+	@GetMapping(path = "/id/{id}")
+	public ResponseEntity<FoodEntity> getFoodByID(@PathVariable("id") Long id) {
+		FoodEntity food = service.getFoodByID(id);
+		ResponseEntity<FoodEntity> response = ResponseEntity.ok(food);
+		return response;
+	}
+}
