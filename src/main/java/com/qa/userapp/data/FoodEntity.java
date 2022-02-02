418e09d35378bcb3f8a81f9edb6930086bee2f1d
london-ay <aysun.uk2017@gmail.com>
2022-02-02 10:05:06 +0000

    generated methods

--- src/main/java/com/qa/userapp/data/FoodEntity.java
+++ src/main/java/com/qa/userapp/data/FoodEntity.java
@@ -24,4 +24,59 @@ public class FoodEntity {
 	
 	@Column
 	private float protein;
+
+	public FoodEntity(Long id, String name, float carbs, float fat, float protein) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.carbs = carbs;
+		this.fat = fat;
+		this.protein = protein;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getCarbs() {
+		return carbs;
+	}
+
+	public void setCarbs(float carbs) {
+		this.carbs = carbs;
+	}
+
+	public float getFat() {
+		return fat;
+	}
+
+	public void setFat(float fat) {
+		this.fat = fat;
+	}
+
+	public float getProtein() {
+		return protein;
+	}
+
+	public void setProtein(float protein) {
+		this.protein = protein;
+	}
+
+	@Override
+	public String toString() {
+		return "FoodEntity [id=" + id + ", name=" + name + ", carbs=" + carbs + ", fat=" + fat + ", protein=" + protein
+				+ "]";
+	}
 }
