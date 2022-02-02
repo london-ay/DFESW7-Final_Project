6c9d8f4bfd5065d67187a3e634de970e4ed1cfac
london-ay <aysun.uk2017@gmail.com>
2022-02-02 21:26:33 +0000

    added put and delete mappings

--- src/main/java/com/qa/userapp/controller/FoodController.java
+++ src/main/java/com/qa/userapp/controller/FoodController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,10 +47,25 @@ public class FoodController {
 		return response;
 	}
 	
-	@PostMapping(path = "/create")
+	@PostMapping
 	public ResponseEntity<FoodEntity> createFood(@RequestBody FoodEntity food) {
 		FoodEntity savedFood = this.service.createFood(food);
 		ResponseEntity<FoodEntity> response = ResponseEntity.ok(savedFood);
+		return response;
+	}
+	
+	@PutMapping(path = "/id/{id}")
+	public ResponseEntity<FoodEntity> updateFood(@PathVariable("id") Long id, @RequestBody FoodEntity food) {
+		FoodEntity updatedFood = this.service.updateFood(food, id);
+		ResponseEntity<FoodEntity> response = ResponseEntity.ok(updatedFood); 
+		return response;
+	}
+	
+	@DeleteMapping(path = "/id/{id}")
+	public ResponseEntity<List<FoodEntity>> deleteFood(@PathVariable("id") Long id) {
+		this.service.deleteFoodByID(id);
+		List<FoodEntity> foods = this.service.getAllFoods();
+		ResponseEntity<List<FoodEntity>> response = ResponseEntity.ok(foods);
 		return response;
 	}
 }
