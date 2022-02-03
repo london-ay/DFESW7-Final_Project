86a38c259ff23bfb7d59087d34346e1b2a30ebc7
london-ay <aysun.uk2017@gmail.com>
2022-02-03 14:41:29 +0000

    added more service tests

--- src/test/java/com/qa/userapp/service/FoodServiceTest.java
+++ src/test/java/com/qa/userapp/service/FoodServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,10 +28,34 @@ public class FoodServiceTest {
 	@Test
 	public void testGetAllFoods() {
 		ArrayList<FoodEntity> foods = new ArrayList<FoodEntity>();
-		FoodEntity food1 = new FoodEntity(Long.valueOf(1), "cacao", (float) 20.00, (float) 12.45, (float) 9.30);
-		FoodEntity food2 = new FoodEntity(Long.valueOf(2), "rice", (float) 15.00, (float) 7.45, (float) 8.30);
-		foods.addAll(List.of(food1, food2));
+		FoodEntity cacao = new FoodEntity(Long.valueOf(1), "cacao", (float) 20.00, (float) 12.45, (float) 9.30);
+		FoodEntity rice = new FoodEntity(Long.valueOf(2), "rice", (float) 15.00, (float) 7.45, (float) 8.30);
+		foods.addAll(List.of(cacao, rice));
 		when(this.repository.findAll()).thenReturn(foods);
 		assertThat(this.service.getAllFoods()).isEqualTo(foods);
+	}
+	
+	@Test
+	public void testGetFoodByID() {
+		Long id = Long.valueOf(1);
+		FoodEntity cacao = new FoodEntity(id, "cacao", (float) 20.00, (float) 12.45, (float) 9.30);
+		when(this.repository.findById(id)).thenReturn(Optional.of(cacao));
+		assertThat(this.service.getFoodByID(id)).isEqualTo(cacao);
+	}
+	
+	@Test
+	public void testCreateFood() {
+		Long id = Long.valueOf(1);
+		FoodEntity cacao = new FoodEntity(id, "cacao", (float) 20.00, (float) 12.45, (float) 9.30);
+		when(this.repository.save(cacao)).thenReturn(cacao);
+		assertThat(this.service.createFood(cacao)).isEqualTo(cacao);
+	}
+	
+	@Test
+	public void testUpdateFood() {
+		Long id = Long.valueOf(1);
+		FoodEntity cacao = new FoodEntity(id, "cacao", (float) 20.00, (float) 12.45, (float) 9.30);
+		when(this.repository.findById(id)).thenReturn(Optional.of(cacao));
+		assertThat(service.updateFood(cacao, id)).isEqualTo(cacao);
 	}
 }
